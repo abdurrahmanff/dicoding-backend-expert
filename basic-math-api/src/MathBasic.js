9f5f0f6f185cb021bf50ab98ff2980e18f6dad16
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-05-17 22:21:56 +0700

    feat: check parameters type

--- basic-math-api/src/MathBasic.js
+++ basic-math-api/src/MathBasic.js
@@ -3,6 +3,12 @@ const MathBasic = {
     if (args.length !== 2) {
       throw new Error('Add function only receive two parameters');
     }
+
+    const [a, b] = args;
+
+    if (typeof a !== 'number' || typeof b !== 'number') {
+      throw new Error('Add function only receive number as parameters');
+    }
   },
   subtract: () => {
 
