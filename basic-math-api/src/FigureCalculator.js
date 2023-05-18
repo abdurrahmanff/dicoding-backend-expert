dfddf56d8b770283b8085daa089dcc2759853da6
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-05-18 14:13:56 +0700

    refactor: extract arguments validation to different method

--- basic-math-api/src/FigureCalculator.js
+++ basic-math-api/src/FigureCalculator.js
@@ -3,58 +3,40 @@ class FigureCalculator {
     this.mathBasic = mathBasic;
   }
 
+  static validateArguments(args, expectedLength) {
+    if (args.length !== expectedLength) {
+      throw new Error(`Functions only receive ${expectedLength} parameters`);
+    }
+
+    args.forEach((arg) => {
+      if (typeof arg !== 'number') {
+        throw new Error('Functions only receive number as parameter');
+      }
+    });
+
+    return args;
+  }
+
   calculateRectanglePerimeter(...args) {
-    if (args.length !== 2) {
-      throw new Error('Functions only receive 2 parameters');
-    }
-
-    const [length, width] = args;
-
-    if (typeof length !== 'number' || typeof width !== 'number') {
-      throw new Error('Functions only receive number as parameter');
-    }
+    const [length, width] = FigureCalculator.validateArguments(args, 2);
 
     return this.mathBasic.multiply(2, this.mathBasic.add(length, width));
   }
 
   calculateRectangleArea(...args) {
-    if (args.length !== 2) {
-      throw new Error('Functions only receive 2 parameters');
-    }
-
-    const [length, width] = args;
-
-    if (typeof length !== 'number' || typeof width !== 'number') {
-      throw new Error('Functions only receive number as parameter');
-    }
+    const [length, width] = FigureCalculator.validateArguments(args, 2);
 
     return this.mathBasic.multiply(length, width);
   }
 
   calculateTrianglePerimeter(...args) {
-    if (args.length !== 3) {
-      throw new Error('Functions only receive 3 parameters');
-    }
-
-    const [a, b, c] = args;
-
-    if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') {
-      throw new Error('Functions only receive number as parameter');
-    }
+    const [a, b, c] = FigureCalculator.validateArguments(args, 3);
 
     return this.mathBasic.add(this.mathBasic.add(a, b), c);
   }
 
   calculateTriangleArea(...args) {
-    if (args.length !== 2) {
-      throw new Error('Functions only receive 2 parameters');
-    }
-
-    const [base, height] = args;
-
-    if (typeof base !== 'number' || typeof height !== 'number') {
-      throw new Error('Functions only receive number as parameter');
-    }
+    const [base, height] = FigureCalculator.validateArguments(args, 2);
 
     return this.mathBasic.divide(this.mathBasic.multiply(base, height), 2);
   }
