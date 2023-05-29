1ead1717b7681db838da1a06b88b3d57b4c407b3
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-05-29 21:43:57 +0700

    chore: forgot async

--- forum-api/src/Domains/threads/ThreadRepository.js
+++ forum-api/src/Domains/threads/ThreadRepository.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 class ThreadRepository {
-  addThread(storeThread) {
+  async addThread(storeThread) {
     throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
 }
