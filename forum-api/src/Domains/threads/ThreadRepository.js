937c02bcb9e59a91c47d05178054d21c70136154
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-05-29 21:39:19 +0700

    chore: create thread repo interface

---
+++ forum-api/src/Domains/threads/ThreadRepository.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+class ThreadRepository {
+  addThread(storeThread) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = ThreadRepository;
