de1b398fc2db6b10388357596587e934cf785200
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-05-29 14:04:40 +0700

    chore: create table threads migration

---
+++ forum-api/migrations/1685342704324_create-table-threads.js
@@ -0,0 +1,38 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('threads', {
+    id: {
+      type: 'VARHCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    body: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    date: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
+
+  pgm.addConstraint(
+    'threads',
+    'fk_threads.user_id_user.id',
+    'FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE',
+  );
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('threads');
+};
