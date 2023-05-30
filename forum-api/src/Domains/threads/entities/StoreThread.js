93144bf963b76ba5181107bce5df208dbe55ec15
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-05-30 14:54:53 +0700

    chore: add payload verification

--- forum-api/src/Domains/threads/entities/StoreThread.js
+++ forum-api/src/Domains/threads/entities/StoreThread.js
@@ -1,8 +1,23 @@
 class StoreThread {
-  constructor({ title, body, owner }) {
+  constructor(payload) {
+    this.verifyPayload(payload);
+
+    const {
+      id, title, body, owner,
+    } = payload;
+
+    this.id = id;
     this.title = title;
     this.body = body;
     this.owner = owner;
+  }
+
+  verifyPayload(payload) {
+    const { title } = payload;
+
+    if (title.length > 50) {
+      throw new Error('STORE_THREAD.TITLE_LIMIT_CHAR');
+    }
   }
 }
 
