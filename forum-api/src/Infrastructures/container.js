b33c57b02d0a32dcbffa49dc7591edc907ac2d60
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-05-31 15:00:14 +0700

    fix: forget to inject add thread use case

--- forum-api/src/Infrastructures/container.js
+++ forum-api/src/Infrastructures/container.js
@@ -26,6 +26,7 @@ const AuthenticationRepository = require('../Domains/authentications/Authenticat
 const AuthenticationRepositoryPostgres = require('./repository/AuthenticationRepositoryPostgres');
 const LogoutUserUseCase = require('../Applications/use_case/LogoutUserUseCase');
 const RefreshAuthenticationUseCase = require('../Applications/use_case/RefreshAuthenticationUseCase');
+const AddThreadUseCase = require('../Applications/use_case/AddThreadUseCase');
 
 // creating container
 const container = createContainer();
@@ -163,6 +164,23 @@ container.register([
         {
           name: 'authenticationTokenManager',
           internal: AuthenticationTokenManager.name,
+        },
+      ],
+    },
+  },
+  {
+    key: AddThreadUseCase.name,
+    Class: AddThreadUseCase,
+    parameter: {
+      injectType: 'destructuring',
+      dependencies: [
+        {
+          name: 'threadRepository',
+          internal: ThreadRepository.name,
+        },
+        {
+          name: 'idGenerator',
+          concrete: nanoid,
         },
       ],
     },
