b56b3b9b39a8b45fab228891c781a3d4d85e2e2a
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-06-05 15:46:32 +0700

    feat: add comments on thread

--- forum-api/src/Interfaces/http/api/threads/handler.js
+++ forum-api/src/Interfaces/http/api/threads/handler.js
@@ -1,5 +1,6 @@
 const autoBind = require('auto-bind');
 const AddThreadUseCase = require('../../../../Applications/use_case/AddThreadUseCase');
+const AddCommentUseCase = require('../../../../Applications/use_case/AddCommentUseCase');
 
 class ThreadsHandler {
   constructor(container) {
@@ -19,6 +20,24 @@ class ThreadsHandler {
       status: 'success',
       data: {
         addedThread,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  async postCommentOnThreadById(request, h) {
+    const addCommentUseCase = this.container.getInstance(AddCommentUseCase.name);
+
+    const { threadId } = request.params;
+    const { id: userId } = request.auth.credentials;
+
+    const addedComment = await addCommentUseCase.execute({ ...request.payload, threadId, userId });
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        addedComment,
       },
     });
     response.code(201);
