43007c1d882c8fc1da579006c5f28623ca2538e7
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-06-05 15:54:40 +0700

    chore: add column `deleted` on `comments` for delete comment use case

---
+++ forum-api/migrations/1685954894424_add-column-status-into-table-comments.js
@@ -0,0 +1,17 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.addColumn('comments', {
+    deleted: {
+      type: 'boolean',
+    },
+  });
+
+  pgm.sql('UPDATE comments SET deleted=FALSE WHERE deleted IS NULL');
+};
+
+exports.down = (pgm) => {
+  pgm.dropColumn('comments', 'deleted');
+};
