3f95bf9949d5467ac62cdd51d530b6e8904241b2
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-06-05 14:57:27 +0700

    chore: add method to verify thread exist

--- forum-api/src/Domains/threads/ThreadRepository.js
+++ forum-api/src/Domains/threads/ThreadRepository.js
@@ -3,6 +3,10 @@ class ThreadRepository {
   async addThread(storeThread) {
     throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
+
+  async verifyThreadExist(threadId) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 }
 
 module.exports = ThreadRepository;
