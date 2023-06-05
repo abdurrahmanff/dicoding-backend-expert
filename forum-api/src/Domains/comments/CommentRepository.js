c8c04ee6cb7e7237d62ddac5ab97a2acc094b931
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-06-05 16:02:04 +0700

    chore: add method to delete comment

--- forum-api/src/Domains/comments/CommentRepository.js
+++ forum-api/src/Domains/comments/CommentRepository.js
@@ -7,6 +7,10 @@ class CommentRepository {
   async verifyUserComment(commentId, userId) {
     throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
+
+  async deleteComment(id) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 }
 
 module.exports = CommentRepository;
