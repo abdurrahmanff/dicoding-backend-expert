de2b2991fc8136e4ec45e6d7b94e31ea7f4d1b45
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-06-06 00:08:59 +0700

    chore: create detail comment entity

---
+++ forum-api/src/Domains/comments/entities/DetailComment.js
@@ -0,0 +1,12 @@
+class DetailComment {
+  constructor({
+    id, username, date, content,
+  }) {
+    this.id = id;
+    this.username = username;
+    this.date = date;
+    this.content = content;
+  }
+}
+
+module.exports = DetailComment;
