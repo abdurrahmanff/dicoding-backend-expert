891851f73cae9bdb0f9fd7208d0452d75eab618b
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-06-06 22:39:56 +0700

    chore: implements action flow of get detail thread use case

---
+++ forum-api/src/Applications/use_case/GetDetailThreadUseCase.js
@@ -0,0 +1,29 @@
+class GetDetailThread {
+  constructor({ threadRepository }) {
+    this.threadRepository = threadRepository;
+  }
+
+  async execute(useCasePayload) {
+    this.verifyUseCasePayload(useCasePayload);
+
+    const { threadId } = useCasePayload;
+
+    const detailThread = this.threadRepository.getDetailThreadById(threadId);
+
+    return detailThread;
+  }
+
+  verifyUseCasePayload(useCasePayload) {
+    const { threadId } = useCasePayload;
+
+    if (!threadId) {
+      throw new Error('DETAIL_THREAD.NOT_CONTAIN_NEEDED_PROPERTIES');
+    }
+
+    if (typeof threadId !== 'string') {
+      throw new Error('DETAIL_THREAD.DID_NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
+  }
+}
+
+module.exports = GetDetailThread;
