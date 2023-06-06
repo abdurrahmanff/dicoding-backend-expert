e51675f3c6c1767036adabfd35224a43ed8a9fab
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-06-06 23:24:33 +0700

    chore: implements `getDetailThreadById` method using postgres

--- forum-api/src/Infrastructures/repository/ThreadRepositoryPostgres.js
+++ forum-api/src/Infrastructures/repository/ThreadRepositoryPostgres.js
@@ -1,5 +1,7 @@
 const NotFoundError = require('../../Commons/exceptions/NotFoundError');
+const DetailComment = require('../../Domains/comments/entities/DetailComment');
 const ThreadRepository = require('../../Domains/threads/ThreadRepository');
+const DetailThread = require('../../Domains/threads/entities/DetailThread');
 const StoreThread = require('../../Domains/threads/entities/StoreThread');
 
 class ThreadRepositoryPostgres extends ThreadRepository {
@@ -36,6 +38,43 @@ class ThreadRepositoryPostgres extends ThreadRepository {
     if (!result.rowCount) {
       throw new NotFoundError('thread tidak ditemukan');
     }
+  }
+
+  async getDetailThreadById(id) {
+    await this.verifyThreadExist(id);
+
+    const query = {
+      text: `SELECT t.id, t.title, t.body, t.date, ut.username AS t_user, c.id AS c_id, uc.username AS c_user, c.date AS c_date, c.content
+      FROM threads AS t
+      JOIN users AS ut ON t.user_id = ut.id
+      LEFT JOIN comments AS c ON t.id = c.thread_id
+      JOIN users AS uc ON c.user_id = uc.id 
+      WHERE t.id = $1`,
+      values: [id],
+    };
+
+    const result = await this.pool.query(query);
+
+    // if (result) {
+    //   throw new Error(JSON.stringify(result.rows));
+    // }
+    const comments = result.rows.map((row) => new DetailComment({
+      id: row.c_id,
+      username: row.c_user,
+      date: row.c_date,
+      content: row.content,
+    }));
+
+    const detailThread = new DetailThread({
+      id: result.rows[0].id,
+      title: result.rows[0].title,
+      body: result.rows[0].body,
+      date: result.rows[0].date,
+      username: result.rows[0].t_user,
+      comments,
+    });
+
+    return detailThread;
   }
 }
 
--- forum-api/src/Infrastructures/repository/test/ThreadRepositoryPostgres.test.js
+++ forum-api/src/Infrastructures/repository/test/ThreadRepositoryPostgres.test.js
@@ -3,6 +3,9 @@ const pool = require('../../database/postgres/pool');
 const StoreThread = require('../../../Domains/threads/entities/StoreThread');
 const ThreadRepositoryPostgres = require('../ThreadRepositoryPostgres');
 const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const CommentTableTestHelper = require('../../../../tests/CommentsTableTestHelper');
+const DetailThread = require('../../../Domains/threads/entities/DetailThread');
+const DetailComment = require('../../../Domains/comments/entities/DetailComment');
 
 describe('ThreadRepository postgres implementation test', () => {
   afterEach(async () => {
@@ -55,7 +58,7 @@ describe('ThreadRepository postgres implementation test', () => {
     });
   });
 
-  describe('verifyyThreadExist function', () => {
+  describe('verifyThreadExist function', () => {
     it('should throw error when thread not exist', async () => {
       const threadId = 'thread-133';
 
@@ -67,6 +70,52 @@ describe('ThreadRepository postgres implementation test', () => {
       await expect(threadRepositoryPostgres.verifyThreadExist(threadId))
         .rejects
         .toThrowError('thread tidak ditemukan');
+    });
+  });
+
+  describe('getDetailThreadById function', () => {
+    it('should return detail thread correctly', async () => {
+      const threadId = 'thread-123';
+
+      const threadRepositoryPostgres = new ThreadRepositoryPostgres(pool);
+
+      const date = new Date().toISOString();
+
+      await UsersTableTestHelper.addUser({ id: 'user-123', username: 'user A' });
+      await UsersTableTestHelper.addUser({ id: 'user-124', username: 'user B' });
+      await ThreadsTableTestHelper.addThread({
+        id: 'thread-123', userId: 'user-123', date, title: 'judul', body: 'isi thread',
+      });
+      await CommentTableTestHelper.addComment({
+        id: 'comment-123', threadId: 'thread-123', date, userId: 'user-124', content: 'komentar 1',
+      });
+      await CommentTableTestHelper.addComment({
+        id: 'comment-124', threadId: 'thread-123', date, userId: 'user-124', content: 'komentar 2',
+      });
+
+      const detailThread = await threadRepositoryPostgres.getDetailThreadById(threadId);
+
+      expect(detailThread).toStrictEqual(new DetailThread({
+        id: threadId,
+        title: 'judul',
+        body: 'isi thread',
+        date,
+        username: 'user A',
+        comments: [
+          new DetailComment({
+            id: 'comment-123',
+            username: 'user B',
+            date,
+            content: 'komentar 1',
+          }),
+          new DetailComment({
+            id: 'comment-124',
+            username: 'user B',
+            date,
+            content: 'komentar 2',
+          }),
+        ],
+      }));
     });
   });
 });
