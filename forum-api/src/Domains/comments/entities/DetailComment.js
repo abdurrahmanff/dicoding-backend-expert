2421fb9873cc4ed92b0b1820bd9548ef7d496476
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-06-09 22:48:46 +0700

    chore: redefine entity based on domain model

--- forum-api/src/Domains/comments/entities/DetailComment.js
+++ forum-api/src/Domains/comments/entities/DetailComment.js
@@ -1,12 +1,20 @@
 class DetailComment {
   constructor({
-    id, username, date, content, replies,
+    id, username, date, content, deleted, parent,
   }) {
     this.id = id;
     this.username = username;
     this.date = date;
     this.content = content;
-    this.replies = replies;
+    this.deleted = deleted;
+    this.parent = parent;
+  }
+
+  getContent() {
+    // eslint-disable-next-line no-nested-ternary
+    return this.deleted
+      ? this.parent ? '**balasan telah dihapus**'
+        : '**komentar telah dihapus**' : this.content;
   }
 }
 
--- forum-api/src/Domains/comments/entities/test/DetailComment.test.js
+++ forum-api/src/Domains/comments/entities/test/DetailComment.test.js
@@ -7,7 +7,8 @@ describe('DetailComment test', () => {
       username: 'username',
       date: 'now',
       content: 'isi komentar',
-      replies: ['a', 'b'],
+      deleted: true,
+      parent: null,
     };
 
     const detailComment = new DetailComment(payload);
@@ -16,6 +17,54 @@ describe('DetailComment test', () => {
     expect(detailComment.username).toEqual(payload.username);
     expect(detailComment.date).toEqual(payload.date);
     expect(detailComment.content).toEqual(payload.content);
-    expect(detailComment.replies).toEqual(payload.replies);
+    expect(detailComment.deleted).toEqual(payload.deleted);
+    expect(detailComment.parent).toEqual(payload.parent);
+  });
+
+  describe('getContent function', () => {
+    it('should return its value if comment/reply is not deleted', () => {
+      const payload = {
+        id: 'comment-123',
+        username: 'username',
+        date: 'now',
+        content: 'isi komentar',
+        deleted: false,
+        parent: null,
+      };
+
+      const detailComment = new DetailComment(payload);
+
+      expect(detailComment.getContent()).toEqual(payload.content);
+    });
+
+    it('should return "**komentar telah dihapus**" if comment is deleted', () => {
+      const payload = {
+        id: 'comment-123',
+        username: 'username',
+        date: 'now',
+        content: 'isi komentar',
+        deleted: true,
+        parent: null,
+      };
+
+      const detailComment = new DetailComment(payload);
+
+      expect(detailComment.getContent()).toEqual('**komentar telah dihapus**');
+    });
+
+    it('should return "**balasan telah dihapus**" if reply is not deleted', () => {
+      const payload = {
+        id: 'reply-123',
+        username: 'username',
+        date: 'now',
+        content: 'isi komentar',
+        deleted: true,
+        parent: 'comment-123',
+      };
+
+      const detailComment = new DetailComment(payload);
+
+      expect(detailComment.getContent()).toEqual('**balasan telah dihapus**');
+    });
   });
 });
