982b399915fc0ecf5cf0ccd77a67ef66ab9ac2c3
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-06-12 15:03:31 +0700

    chore: fix useless destructuring

--- forum-api/src/Infrastructures/repository/ThreadRepositoryPostgres.js
+++ forum-api/src/Infrastructures/repository/ThreadRepositoryPostgres.js
@@ -23,7 +23,7 @@ class ThreadRepositoryPostgres extends ThreadRepository {
 
     const result = await this.pool.query(query);
 
-    return new StoreThread({ ...result.rows[0] });
+    return new StoreThread(result.rows[0]);
   }
 
   async verifyThreadExist(threadId) {
