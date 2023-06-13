a2b66acd1c84679fa2ac6f005b7e08d4582a5aed
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-06-13 14:09:25 +0700

    test: adjust property name

--- forum-api/src/Domains/likes/entities/test/StoreLike.test.js
+++ forum-api/src/Domains/likes/entities/test/StoreLike.test.js
@@ -5,13 +5,13 @@ describe('StoreLike', () => {
     const payload = {
       id: 'like-123',
       commentId: 'comment-123',
-      owner: 'user-123',
+      userId: 'user-123',
     };
 
     const storeLike = new StoreLike(payload);
 
     expect(storeLike.id).toEqual(payload.id);
     expect(storeLike.commentId).toEqual(payload.commentId);
-    expect(storeLike.owner).toEqual(payload.owner);
+    expect(storeLike.userId).toEqual(payload.owner);
   });
 });
