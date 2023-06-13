d3a4e10d38709bc1dd1c7eeab6fc6ce0223a0a49
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-06-13 19:13:50 +0700

    chore: create store like entity

---
+++ forum-api/src/Domains/likes/entities/StoreLike.js
@@ -0,0 +1,11 @@
+class StoreLike {
+  constructor({
+    id, commentId, userId,
+  }) {
+    this.id = id;
+    this.commentId = commentId;
+    this.userId = userId;
+  }
+}
+
+module.exports = StoreLike;
