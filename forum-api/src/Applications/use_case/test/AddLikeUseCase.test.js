f8bc96492ac926dba7c2d40be07ef03e23344671
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-06-13 22:43:30 +0700

    test: fix error

--- forum-api/src/Applications/use_case/test/AddLikeUseCase.test.js
+++ forum-api/src/Applications/use_case/test/AddLikeUseCase.test.js
@@ -3,14 +3,16 @@ const LikeRepository = require('../../../Domains/likes/LikeRepository');
 const StoreLike = require('../../../Domains/likes/entities/StoreLike');
 
 describe('AddLikeUseCase', () => {
-  describe('should orchestrating remove like action correctly', async () => {
+  it('should orchestrating remove like action correctly', async () => {
     const useCasePayload = {
       commentId: 'comment-123',
       userId: 'user-123',
     };
 
     const mockLikeRepository = new LikeRepository();
-    mockLikeRepository.verifyLikeNotExist = jest.fn(() => Promise.reject());
+    mockLikeRepository.verifyLikeNotExist = jest.fn(
+      () => Promise.reject(new Error('LIKE_REPOSITORY.COMMENT_HAVE_BEEN_LIKED_BY_USER')),
+    );
     mockLikeRepository.removeLike = jest.fn(() => Promise.resolve());
 
     const addLikeUseCase = new AddLikeUseCase({
@@ -25,7 +27,7 @@ describe('AddLikeUseCase', () => {
       .toBeCalledWith(useCasePayload.commentId, useCasePayload.userId);
   });
 
-  describe('should orchestrating add like action correctly', async () => {
+  it('should orchestrating add like action correctly', async () => {
     const useCasePayload = {
       commentId: 'comment-123',
       userId: 'user-123',
