1e911e3c3f693034b28574008be489bc406eab1b
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-06-14 10:48:00 +0700

    chore: add missing dependency on `GetDetailThreadUseCase`

--- forum-api/src/Infrastructures/container.js
+++ forum-api/src/Infrastructures/container.js
@@ -264,6 +264,10 @@ container.register([
           name: 'commentRepository',
           internal: CommentRepository.name,
         },
+        {
+          name: 'likeRepository',
+          internal: LikeRepository.name,
+        },
       ],
     },
   },
